484187b082a8fadc1effe182da6e6b29b8047f45
Camilo Crespo <crezam@live.com>
2016-11-07 00:57:58 -0500

    Refactor core logic to send/receive messages to Kafka into goctopus own library so it can be reused as framework
    Reference:
    https://medium.com/@benbjohnson/structuring-applications-in-go-3b04be4ff091#.rfxpeh64b
    http://dave.cheney.net/2014/12/01/five-suggestions-for-setting-up-a-go-project

--- connectors/kafka.go
+++ connectors/kafka.go
@@ -1,1 +1,80 @@
 package connectors
+
+import (
+	"github.com/Shopify/sarama"
+	"log" // TODO: use logrus
+	"time"
+)
+
+type KafkaConnector struct {
+	kafkaURL   string
+	kafkaTopic string
+}
+
+func (kc *KafkaConnector) send(request RequestMessage) {
+
+	producer, err := sarama.NewAsyncProducer([]string{kc.kafkaURL}, nil)
+
+	if err != nil {
+		//Retry a few times before going into panic
+		for retries := 1; retries <= 5; retries++ {
+			producer, err = sarama.NewAsyncProducer([]string{kc.kafkaURL}, nil)
+
+			if err != nil {
+				time.Sleep(100 * time.Millisecond)
+			} else {
+				break
+			}
+		}
+	}
+
+	//Deferred function to close producer
+	defer func() {
+		producer.AsyncClose()
+	}()
+
+	// send  a message synchronously when send method is called
+	msg := &sarama.ProducerMessage{Topic: kc.kafkaTopic, Value: sarama.StringEncoder(request)}
+	producer.Input() <- msg
+}
+
+func (kc *KafkaConnector) receive() ResponseMessage {
+
+	consumer, err := sarama.NewConsumer([]string{kc.kafkaURL}, nil)
+	if err != nil {
+		//Retry a few times before going into panic
+		for retries := 1; retries <= 5; retries++ {
+			consumer, err = sarama.NewConsumer([]string{kc.kafkaURL}, nil)
+
+			if err != nil {
+				time.Sleep(100 * time.Millisecond)
+			} else {
+				break
+			}
+		}
+	}
+
+	//Deferred function to close consumer
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	partitionConsumer, err := consumer.ConsumePartition(kc.kafkaTopic, 0, sarama.OffsetNewest)
+	if err != nil {
+		panic(err)
+	}
+
+	//Deferred function to close partition consumer
+	defer func() {
+		if err := partitionConsumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	// receive one message synchronously on each receive call of the KafkaConnector
+	msg := <-partitionConsumer.Messages()
+
+	return string(msg.Value())
+}
