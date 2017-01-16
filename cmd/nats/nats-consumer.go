97545225c0fc978b7eb8b76219b3e75406c632c3
Camilo Crespo <crezam@live.com>
2017-01-16 01:34:20 -0500

    Sample consumer

--- cmd/nats/nats-consumer.go
+++ cmd/nats/nats-consumer.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/nats-io/go-nats"
+	"github.com/stupendous-man/goctupus/connectors"
+	"log"
+)
+
+func main() {
+	conn := connectors.NatsConnector{NatsUrl: nats.DefaultURL, NatsSubject: "foo.bar"}
+	msg := conn.Receive()
+	log.Println(msg)
+}
