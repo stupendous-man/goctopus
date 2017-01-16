860d32480700973dcecf3a969cf88901f46560f0
Camilo Crespo <crezam@live.com>
2017-01-16 01:10:02 -0500

    Add sender

--- connectors/nats.go
+++ connectors/nats.go
@@ -1,4 +1,35 @@
 package connectors
 
+import (
+	"github.com/nats-io/go-nats"
+	"log"
+)
+
 type NatsConnector struct {
+	NatsUrl     string
+	NatsSubject string
 }
+
+func (nc *NatsConnector) Send(request RequestMessage) {
+	nconn, err := nats.Connect(nc.NatsUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nconn.Close()
+
+	nconn.Publish(nc.NatsSubject, []byte(request))
+	nconn.Flush()
+
+	if err := nconn.LastError(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Sent successful")
+	}
+}
+
+// TODO
+/*
+func (nc *NatsConnector) Receive() ResponseMessage {
+
+}
+*/
