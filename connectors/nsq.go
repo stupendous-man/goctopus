fc58b460be3f2b794d631cdce2ab417622cddb38
Camilo Crespo <crezam@live.com>
2017-01-19 01:00:19 -0500

    NSQ Producer

--- connectors/nsq.go
+++ connectors/nsq.go
@@ -1,1 +1,25 @@
 package connectors
+
+import "github.com/nsqio/go-nsq"
+
+type NsqConnector struct {
+	NsqUrl   string
+	NsqTopic string
+}
+
+func (nc *NsqConnector) Send(request RequestMessage) {
+
+	producer, _ := nsq.NewProducer(nc.NsqUrl, nsq.NewConfig())
+
+	err := producer.Publish(nc.NsqTopic, []byte(request))
+	if err != nil {
+		panic(err)
+	}
+
+	producer.Stop()
+}
+
+func (nc *NsqConnector) Receive() ResponseMessage {
+
+	return nil
+}
