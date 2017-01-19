a3929f55e82a9f16ea8db3abb22a5a8375d50673
Camilo Crespo <crezam@live.com>
2017-01-19 01:30:48 -0500

    NSQ Consumer

--- connectors/nsq.go
+++ connectors/nsq.go
@@ -1,10 +1,14 @@
 package connectors
 
-import "github.com/nsqio/go-nsq"
+import (
+	"github.com/nsqio/go-nsq"
+	"sync"
+)
 
 type NsqConnector struct {
-	NsqUrl   string
-	NsqTopic string
+	NsqUrl     string
+	NsqTopic   string
+	NsqChannel string
 }
 
 func (nc *NsqConnector) Send(request RequestMessage) {
@@ -21,5 +25,27 @@ func (nc *NsqConnector) Send(request RequestMessage) {
 
 func (nc *NsqConnector) Receive() ResponseMessage {
 
-	return nil
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	consumer, _ := nsq.NewConsumer(nc.NsqTopic, nc.NsqChannel, nsq.NewConfig())
+
+	var response ResponseMessage
+
+	consumer.AddHandler(
+		nsq.HandlerFunc(
+			func(message *nsq.Message) error {
+				response = ResponseMessage(string(message.Body))
+				wg.Done()
+				return nil
+			}))
+
+	err := consumer.ConnectToNSQLookupd(nc.NsqUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	wg.Wait()
+
+	return response
 }
